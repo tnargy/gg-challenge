ecc9e6b6381ad123feedd60fa059ef0f77619637
Grant <clsscmstng@gmail.com>
2024-11-16 09:57:14 -0600

    added touch

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -5,3 +5,16 @@ extends CanvasLayer
 func _process(_delta):
 	%Level.text = str(level.level_number)
 	%Chips.text = str(level.chips_needed)
+
+
+func _on_up_pressed():
+	Input.action_press("Up")
+
+func _on_left_pressed():
+	Input.action_press("Left")
+
+func _on_right_pressed():
+	Input.action_press("Right")
+
+func _on_down_pressed():
+	Input.action_press("Down")
