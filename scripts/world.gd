3d1c3f094b1314623b8f3af636bf7922a7f62332
Grant <clsscmstng@gmail.com>
2024-11-16 19:27:55 -0600

    Starting design of level2

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,14 +1,17 @@
 extends Node2D
 
+@export var current_level: Level
 @onready var timer = $Timer
 var level = 1
 
 func _on_timer_timeout():
-	#remove_child($Level)
-	$Level.queue_free()
+	current_level.queue_free()
 	level += 1
 	var nextLevel = load("res://scenes/levels/level"+str(level)+".tscn")
+	if nextLevel == null:
+		print("Congrats you beat all the levels")
+		return
 	var l = nextLevel.instantiate()
 	add_child(l)
-	l.name = "Level"
+	current_level = l
 	get_tree().paused = false
