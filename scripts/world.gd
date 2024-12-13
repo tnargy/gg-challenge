ded833b540d6cae63a0f0c76c8e435f859498630
tnargy-Desktop <clsscmstng@gmail.com>
2024-12-13 14:23:24 -0600

    Cleaned up Max Level

--- scripts/world.gd
+++ scripts/world.gd
@@ -4,29 +4,25 @@ extends Node2D
 @export var current_level: Level
 @onready var timer = $Timer
 var level: int
-var max_level = 16
 
 
 func _ready():
 	level = load_furthest_level()
-	if level > max_level: level = 1
 	_loadLevel()
 
 func _on_timer_timeout():
-	if level < max_level:
-		level += 1
-		save_furthest_level()
-		_loadLevel()
-	else:
-		print("Congrats you beat all the levels")
+	level += 1
+	save_furthest_level()
+	_loadLevel()
 	
 	
 func _loadLevel():
 	if current_level != null:
 		current_level.queue_free()
-	var nextLevel = load("res://scenes/levels/level"+str(level)+".tscn")
-	if nextLevel == null:
+	if not FileAccess.file_exists("res://scenes/levels/level"+str(level)+".tscn"):
+		print("Congrats you beat all the levels")
 		return
+	var nextLevel = load("res://scenes/levels/level"+str(level)+".tscn")
 	var l = nextLevel.instantiate()
 	add_child(l)
 	current_level = l
@@ -35,6 +31,8 @@ func _loadLevel():
 	get_tree().paused = false
 	
 func save_furthest_level():
+	if not FileAccess.file_exists("res://scenes/levels/level"+str(level)+".tscn"):
+		return
 	var file = FileAccess.open("user://level_data.json", FileAccess.READ_WRITE)
 	if level > int(file.get_as_text()):
 		file.store_string(str(level))
